84fbd0e94d090f0c69588a7f32530b249555c573
gauravbalpande <132254352+gauravbalpande@users.noreply.github.com>
2024-11-18 01:22:03 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-// IMPORTS
 require("dotenv").config();
 const express = require("express");
 const path = require("path");
@@ -18,13 +17,10 @@ const { connectMongoDB } = require("./connect");
 const staticRoute = require("./routes/staticRoute");
 const userRoute = require("./routes/userRoute");
 const cookieParser = require("cookie-parser");
-const {
-  checkForAuthenticationCookie,
-} = require("./middlewares/authentication");
+const { checkForAuthenticationCookie } = require("./middlewares/authentication");
 
-// connection to database
 connectMongoDB(process.env.Mongo_URL).then(() => {
-  // console.log("MongoDB Connected");
+  console.log("MongoDB Connected");
 });
 
 app.set("view engine", "ejs");
@@ -38,12 +34,7 @@ app.get('/:room', (req, res) => {
   res.render('room', { roomId: req.params.room })
 })
 
-//view engine
-
 app.set("views", path.resolve("./views"));
-
-// middlewares
-
 app.use("/user", express.static(path.resolve("public")));
 
 app.use(express.json());
@@ -52,25 +43,9 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(checkForAuthenticationCookie("token"));
 
-// routes
 app.use("/", staticRoute);
 app.use("/user", userRoute);
 
-// socket-io connections
 io.on('connection', socket => {
   socket.on('join-room', (roomId, userId) => {
-    socket.join(roomId)
-    socket.to(roomId).broadcast.emit('user-connected', userId);
-    // messages
-    socket.on('message', (message) => {
-      //send message to the same room
-      io.to(roomId).emit('createMessage', message)
-  }); 
-
-    socket.on('disconnect', () => {
-      socket.to(roomId).broadcast.emit('user-disconnected', userId)
-    })
-  })
-})
-
-server.listen(process.env.PORT||3030)
+    socket.join
