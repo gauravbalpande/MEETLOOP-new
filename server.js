fee8383780b0577539d9c8d262bf97416e164faf
gauravbalpande <132254352+gauravbalpande@users.noreply.github.com>
2024-11-18 01:30:25 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,42 +1,62 @@
-const express = require('express')
-const app = express()
-// const cors = require('cors')
-// app.use(cors())
-const server = require('http').Server(app)
-const io = require('socket.io')(server)
-const { ExpressPeerServer } = require('peer');
+// IMPORTS
+require("dotenv").config();
+const express = require("express");
+const path = require("path");
+const app = express();
+const server = require("http").Server(app);
+const io = require("socket.io")(server);
+const { ExpressPeerServer } = require("peer");
+const { connectMongoDB } = require("./connect");
 const peerServer = ExpressPeerServer(server, {
-  debug: true
+  debug: true,
 });
-const { v4: uuidV4 } = require('uuid')
+const staticRoute = require("./routes/staticRoute");
+const userRoute = require("./routes/userRoute");
+const cookieParser = require("cookie-parser");
+const {
+  checkForAuthenticationCookie,
+} = require("./middlewares/authentication");
 
-app.use('/peerjs', peerServer);
+// connection to database
+connectMongoDB(process.env.Mongo_URL).then(() => {
+  // console.log("MongoDB Connected");
+});
 
-app.set('view engine', 'ejs')
-app.use(express.static('public'))
+//view engine
+app.set("view engine", "ejs");
+app.set("views", path.resolve("./views"));
 
-app.get('/', (req, res) => {
-  res.redirect(`/${uuidV4()}`)
-})
+// middlewares
+app.use(express.static(path.resolve("public")));
+app.use("/user", express.static(path.resolve("public")));
+app.use("/peerjs", peerServer);
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
-app.get('/:room', (req, res) => {
-  res.render('room', { roomId: req.params.room })
-})
+app.use(cookieParser());
+app.use(checkForAuthenticationCookie("token"));
 
-io.on('connection', socket => {
-  socket.on('join-room', (roomId, userId) => {
-    socket.join(roomId)
-    socket.to(roomId).broadcast.emit('user-connected', userId);
-    // messages
-    socket.on('message', (message) => {
-      //send message to the same room
-      io.to(roomId).emit('createMessage', message)
-  }); 
+// routes
+app.use("/", staticRoute);
+app.use("/user", userRoute);
 
-    socket.on('disconnect', () => {
-      socket.to(roomId).broadcast.emit('user-disconnected', userId)
-    })
-  })
-})
+// socket-io connections
+io.on("connection", (socket) => {
+  socket.on("join-room", (roomId, userId) => {
+    socket.join(roomId);
+    socket.broadcast.to(roomId).emit("user-connected", userId);
 
-server.listen(process.env.PORT||3030)
+    socket.on("message", (message) => {
+      io.to(roomId).emit("createMessage", message);
+    });
+
+    socket.on("disconnect", () => {
+      socket.broadcast.to(roomId).emit("user-disconnected", userId);
+    });
+  });
+});
+
+// port listening
+server.listen(process.env.PORT || 3030, () => {
+  // console.log("server started at port:3030");
+});
