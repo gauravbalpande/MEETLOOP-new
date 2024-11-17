ba8d626e4d10e4e053ca42f0a5acd2b0b96ecc3c
gauravbalpande <132254352+gauravbalpande@users.noreply.github.com>
2024-11-18 01:26:45 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,30 +1,19 @@
-require("dotenv").config();
-const express = require("express");
-const path = require("path");
-const app = express();
+const express = require('express')
+const app = express()
+// const cors = require('cors')
+// app.use(cors())
 const server = require('http').Server(app)
 const io = require('socket.io')(server)
 const { ExpressPeerServer } = require('peer');
 const peerServer = ExpressPeerServer(server, {
-  debug: true,
+  debug: true
 });
 const { v4: uuidV4 } = require('uuid')
 
 app.use('/peerjs', peerServer);
 
-const { connectMongoDB } = require("./connect");
-
-const staticRoute = require("./routes/staticRoute");
-const userRoute = require("./routes/userRoute");
-const cookieParser = require("cookie-parser");
-const { checkForAuthenticationCookie } = require("./middlewares/authentication");
-
-connectMongoDB(process.env.Mongo_URL).then(() => {
-  console.log("MongoDB Connected");
-});
-
-app.set("view engine", "ejs");
-app.use(express.static(path.resolve("public")));
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
 
 app.get('/', (req, res) => {
   res.redirect(`/${uuidV4()}`)
@@ -34,18 +23,20 @@ app.get('/:room', (req, res) => {
   res.render('room', { roomId: req.params.room })
 })
 
-app.set("views", path.resolve("./views"));
-app.use("/user", express.static(path.resolve("public")));
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-
-app.use(cookieParser());
-app.use(checkForAuthenticationCookie("token"));
-
-app.use("/", staticRoute);
-app.use("/user", userRoute);
-
 io.on('connection', socket => {
   socket.on('join-room', (roomId, userId) => {
-    socket.join
+    socket.join(roomId)
+    socket.to(roomId).broadcast.emit('user-connected', userId);
+    // messages
+    socket.on('message', (message) => {
+      //send message to the same room
+      io.to(roomId).emit('createMessage', message)
+  }); 
+
+    socket.on('disconnect', () => {
+      socket.to(roomId).broadcast.emit('user-disconnected', userId)
+    })
+  })
+})
+
+server.listen(process.env.PORT||3030)
