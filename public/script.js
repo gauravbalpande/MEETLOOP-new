795c723c1fac5dab25c5325871cdc3d97849891e
gauravbalpande <132254352+gauravbalpande@users.noreply.github.com>
2024-11-18 01:26:13 +0530

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,91 +1,122 @@
-const socket = io('/')
-const videoGrid = document.getElementById('video-grid')
-const peer = new Peer(undefined, {
-  path: '/peerjs',
-  host: '/',
-  port: '443'
-})
-let myVideoStream;
-const myVideo = document.createElement('video')
+const socket = io();
+const videoGrid = document.getElementById("video-grid");
+const myVideo = document.createElement("video");
 myVideo.muted = true;
-const peers = {}
 
-const ROOM_ID = window.location.pathname.split('/')[1]; // Get room ID from URL
+const peers = {};
 
-navigator.mediaDevices.getUserMedia({
-  video: true,
-  audio: true
-}).then(stream => {
-  myVideoStream = stream;
-  addVideoStream(myVideo, stream)
-  peer.on('call', call => {
-    call.answer(stream)
-    const video = document.createElement('video')
-    call.on('stream', userVideoStream => {
-      addVideoStream(video, userVideoStream)
-    })
+var peer = new Peer(undefined, {
+  path: "/peerjs",
+  host: "/",
+  port: "3030",
+});
+
+let myVideoStream;
+navigator.mediaDevices
+  .getUserMedia({
+    video: true,
+    audio: true,
   })
+  .then((stream) => {
+    myVideoStream = stream;
+    addVideoStream(myVideo, stream);
 
-  socket.on('user-connected', userId => {
-    connectToNewUser(userId, stream)
-  })
-  
-  let text = $("input");
-  $('html').keydown(function (e) {
-    if (e.which == 13 && text.val().length !== 0) {
-      socket.emit('message', text.val());
-      text.val('')
-    }
+    peer.on("call", (call) => {
+      call.answer(stream);
+      const video = document.createElement("video");
+      call.on("stream", (userVideoStream) => {
+        addVideoStream(video, userVideoStream);
+      });
+    });
+
+    socket.on("user-connected", (userId) => {
+      setTimeout(() => {
+        connectToNewUser(userId, stream);
+      }, 1000);
+    });
   });
-  socket.on("createMessage", message => {
-    $("ul").append(`<li class="message"><b>user</b><br/>${message}</li>`);
-    scrollToBottom()
-  })
-})
 
-socket.on('user-disconnected', userId => {
-  if (peers[userId]) peers[userId].close()
-})
+socket.on("user-disconnected", (userId) => {
+  if (peers[userId]) peers[userId].close();
+});
 
-peer.on('open', id => {
-  socket.emit('join-room', ROOM_ID, id)
-})
+peer.on("open", (id) => {
+  socket.emit("join-room", ROOMID, id);
+});
 
-function connectToNewUser(userId, stream) {
-  const call = peer.call(userId, stream)
-  const video = document.createElement('video')
-  call.on('stream', userVideoStream => {
-    addVideoStream(video, userVideoStream)
-  })
-  call.on('close', () => {
-    video.remove()
-  })
-  peers[userId] = call
-}
+const connectToNewUser = (userId, stream) => {
+  const call = peer.call(userId, stream);
+  const video = document.createElement("video");
+  call.on("stream", (userVideoStream) => {
+    addVideoStream(video, userVideoStream);
+  });
 
-function addVideoStream(video, stream) {
-  video.srcObject = stream
-  video.addEventListener('loadedmetadata', () => {
-    video.play()
-  })
-  videoGrid.append(video)
-}
+  call.on("close", () => {
+    video.remove();
+    location.reload();
+  });
+
+  peers[userId] = call;
+};
+
+const addVideoStream = (video, stream) => {
+  video.srcObject = stream;
+  video.addEventListener("loadedmetadata", () => {
+    video.play();
+  });
+  videoGrid.append(video);
+};
+
+const inputMsg = document.getElementById("chat_message");
+
+inputMsg.addEventListener("keydown", (e) => {
+  if (e.which == 13 && inputMsg.value.length != 0) {
+    socket.emit("message", inputMsg.value);
+    inputMsg.value = "";
+  }
+});
+
+socket.on("createMessage", (message) => {
+  const msg = document.querySelector(".messages");
+  const messageElement = document.createElement("li");
+  messageElement.classList.add("message");
+  messageElement.innerHTML = `<b>user</b><br/>${message}`;
+  msg.appendChild(messageElement);
+  scrollToBottom();
+});
 
 const scrollToBottom = () => {
-  var d = $('.main__chat_window');
+  const d = document.querySelector(".main_chat_window");
   d.scrollTop(d.prop("scrollHeight"));
-}
+};
 
+// Mute the video
 const muteUnmute = () => {
   const enabled = myVideoStream.getAudioTracks()[0].enabled;
   if (enabled) {
     myVideoStream.getAudioTracks()[0].enabled = false;
     setUnmuteButton();
   } else {
-    setMuteButton();
     myVideoStream.getAudioTracks()[0].enabled = true;
+    setMuteButton();
   }
-}
+};
+
+const setMuteButton = () => {
+  const html = `
+    <i class="fas fa-microphone"></i>
+    <span>Mute</span>
+  `;
+  document.querySelector(".main__mute_button").innerHTML = html;
+};
+
+const setUnmuteButton = () => {
+  const html = `
+    <i class="unmute fas fa-microphone-slash"></i>
+    <span>Unmute</span>
+  `;
+  document.querySelector(".main__mute_button").innerHTML = html;
+};
 
 const playStop = () => {
   let enabled = myVideoStream.getVideoTracks()[0].enabled;
@@ -96,37 +127,21 @@ const playStop = () => {
     setStopVideo();
     myVideoStream.getVideoTracks()[0].enabled = true;
   }
-}
-
-const setMuteButton = () => {
-  const html = `
-    <i class="fas fa-microphone"></i>
-    <span>Mute</span>
-  `
-  document.querySelector('.main__mute_button').innerHTML = html;
-}
-
-const setUnmuteButton = () => {
-  const html = `
-    <i class="unmute fas fa-microphone-slash"></i>
-    <span>Unmute</span>
-  `
-  document.querySelector('.main__mute_button').innerHTML = html;
-}
+};
 
 const setStopVideo = () => {
   const html = `
     <i class="fas fa-video"></i>
     <span>Stop Video</span>
-  `
-  document.querySelector('.main__video_button').innerHTML = html;
-}
+  `;
+  document.querySelector(".main__video_button").innerHTML = html;
+};
 
 const setPlayVideo = () => {
   const html = `
-    <i class="stop fas fa-video-slash"></i>
+  <i class="stop fas fa-video-slash"></i>
     <span>Play Video</span>
-  `
-  document.querySelector('.main__video_button').innerHTML = html;
-}
+  `;
+  document.querySelector(".main__video_button").innerHTML = html;
+};
 
